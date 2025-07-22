0f95a20de938bfe92aa282525428e1a5ad3b094d
premier-groups-devs <system@premierchi.com>
2025-07-22 17:05:35 -0500

    refactor: update GoogleAdsConfig to load credentials from JSON string property instead of file path

--- src/main/java/com/premiergroup/ad_metrics_hub/config/GoogleAdsConfig.java
+++ src/main/java/com/premiergroup/ad_metrics_hub/config/GoogleAdsConfig.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 
 @Configuration(proxyBeanMethods = false)
@@ -21,27 +23,50 @@ public class GoogleAdsConfig {
     @Value("${google.ads.developer-token}")
     private String developerToken;
 
-    @Value("${google.ads.credentials-path}")
-    private String credentialsPath;
+//    @Value("${google.ads.credentials-path}")
+//    private String credentialsPath;
+
+    @Value("${google.ads.credentials-json}")
+    private String credentialsJson;
 
     @Value("${google.ads.mcc-customer-id}")
     private long mccCustomerId;
 
-
+//    @Bean
+//    public GoogleAdsClient getGoogleAdsClientWithServiceAccount() throws IOException {
+//        // Cargar credenciales de la cuenta de servicio desde el archivo JSON
+//        ServiceAccountCredentials serviceAccountCredentials =
+//                (ServiceAccountCredentials) ServiceAccountCredentials.fromStream(
+//                                new FileInputStream(credentialsPath))
+//                        .createScoped(Collections.singleton("https://www.googleapis.com/auth/adwords"));
+//
+//        // Construir GoogleAdsClient con credenciales de la cuenta de servicio usando variables de .env
+//        return GoogleAdsClient.newBuilder()
+//                .setCredentials(serviceAccountCredentials)
+//                .setDeveloperToken(developerToken)
+//                .setLoginCustomerId(mccCustomerId)
+//                .build();
+//    }
 
     @Bean
-    public GoogleAdsClient getGoogleAdsClientWithServiceAccount() throws IOException {
-        // Cargar credenciales de la cuenta de servicio desde el archivo JSON
-        ServiceAccountCredentials serviceAccountCredentials =
-                (ServiceAccountCredentials) ServiceAccountCredentials.fromStream(
-                                new FileInputStream(credentialsPath))
-                        .createScoped(Collections.singleton("https://www.googleapis.com/auth/adwords"));
+    public GoogleAdsClient googleAdsClient() throws IOException {
 
-        // Construir GoogleAdsClient con credenciales de la cuenta de servicio usando variables de .env
-        return GoogleAdsClient.newBuilder()
-                .setCredentials(serviceAccountCredentials)
-                .setDeveloperToken(developerToken)
-                .setLoginCustomerId(mccCustomerId)
-                .build();
+        // turn JSON string into a Stream
+        try (InputStream in = new ByteArrayInputStream(
+                credentialsJson.getBytes(StandardCharsets.UTF_8))) {
+
+            ServiceAccountCredentials serviceAccountCredentials =
+                    (ServiceAccountCredentials) ServiceAccountCredentials
+                            .fromStream(in)
+                            .createScoped(
+                                    Collections.singleton("https://www.googleapis.com/auth/adwords"));
+
+            // Construir GoogleAdsClient con credenciales de la cuenta de servicio usando variables de .env
+            return GoogleAdsClient.newBuilder()
+                    .setCredentials(serviceAccountCredentials)
+                    .setDeveloperToken(developerToken)
+                    .setLoginCustomerId(mccCustomerId)
+                    .build();
+        }
     }
 }
